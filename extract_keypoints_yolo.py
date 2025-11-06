33b1705ee46a75178e9974cfdc43d318d6b62af7
sudo-style <sudostylebusiness@gmail.com>
2025-11-06 12:30:06 -0500

    Branching extract_keypoints_yolo.py instead of per frame basis, store for full video

--- extract_keypoints_yolo.py
+++ extract_keypoints_yolo.py
@@ -5,25 +5,9 @@ import json
 import torch
 import sys
 
-# load YOLO 11 pose model
-model = YOLO("yolo11n-pose.pt")
-
-# path to folder wanting to get poses
-folder_path = "Images/small/hand_fand/hand_fand_Subject1_Tuan_cam-1-index_2.mp4"
-
-pose_data = []
-
-# number of videos
-num_videos = 0
-
-
-#print(os.listdir(folder_path))
-
-
-def get_keypoints(path,frame_id = 0,img_id = 0):
-    model = YOLO("yolo11n-pose.pt")
+def get_keypoints(model, vid_path,vid_id = 0, gesture_id = 0):
     results = model.predict(
-        source=path,
+        source=vid_path,
         save=False,
         show=False,
         conf=0.5,
@@ -38,69 +22,76 @@ def get_keypoints(path,frame_id = 0,img_id = 0):
         # convert tensor to numpy
         kp = result.keypoints.data.cpu().numpy()
 
-        # remove person id > 0
+        # remove person if more than one person
         if len(kp) > 1:
             return pose_data
 
         for person_id, person_keypoints in enumerate(kp):
             frame_entry = {
+                "vid_path": vid_path,
+                "gesture_id": gesture_id,
+                "vid_id": vid_id,
                 "frame_id": frame_id,
                 "person_id": person_id,
-                "img_id": img_id,
-                "keypoints": person_keypoints.tolist()
+                "keypoints": person_keypoints.tolist(),
             }
             pose_data.append(frame_entry)
 
 
     return pose_data
 
-keypoints = get_keypoints(folder_path)
-
-print(len(keypoints))
-
-for i, keypoint in enumerate(keypoints):
-    poses = keypoint["keypoints"]
-    print(i)
-    for pose in poses:
-        print(pose)
-
-
 def main():
-    pass
+    # log how long it takes to run
+    import time
+    start_time = time.time()
+
+    # load YOLO 11 pose model
+    model = YOLO("yolo11n-pose.pt")
+
+    # path to folder wanting to get poses
+    folder_path = "Images/small"
+
+    pose_data = []
+
+    # number of videos
+    num_videos = 0
+
+    gesture_names = sorted(os.listdir(folder_path))
+    gesture_names.remove('.DS_Store')
+    for gesture_id, gesture in enumerate(gesture_names):
+        gesture_path = os.path.join(folder_path, gesture)
+
+        vids = os.listdir(gesture_path)
+        vids.remove('.DS_Store')
+
+
+        pose_sequence = []
+
+        for vid_id, vid in enumerate(vids):
+            vid_path = os.path.join(gesture_path, vid)
+            pose_sequence = get_keypoints(model=model, vid_path=vid_path, vid_id=vid_id, gesture_id=gesture_id)
+
+
+            if pose_sequence:
+                pose_data.append(pose_sequence)
+            else:
+                frame_entry = {
+                    "vid_path": vid_path,
+                    "gesture_id": gesture_id,
+                    "vid_id": vid_id,
+                    "frame_id": 0,
+                    "person_id": 0,
+                    "keypoints": [],
+                }
+                pose_data.append([frame_entry])
+
+        # save the data to json for later use
+        with open("pose_data.json", "w") as f:
+            json.dump(pose_data, f, indent=2)
+
+    print("pose data saved to pose_data.json")
+    end_time = time.time()
+    print("time taken:", end_time - start_time)
 
 if __name__ == "__main__":
     main()
-
-'''
-for gest_id, gesture in enumerate(os.listdir(folder_path)):
-    vids = sorted(os.listdir(folder_path))
-    vids.remove('.DS_Store')
-    for img_id, img in enumerate(vids):
-        frame_id = 0
-
-        # asked gpt for how to make a progress bar
-        bar_length = 30
-        filled_length = int(bar_length * num_videos // 100)
-        bar = '█' * filled_length + '-' * (bar_length - filled_length)
-        sys.stdout.write(f'\rProgress: |{bar}| {num_videos/len(vids) * 100:.2f}%')
-        sys.stdout.flush()
-
-        num_videos += 1
-        print(num_videos)
-
-        img_path = os.path.join(folder_path, img)
-
-
-
-
-
-    # save the data to json for later use
-    with open("pose_data.json", "w") as f:
-        json.dump(pose_data, f, indent=2)
-
-    print("pose data saved to pose_data.json")
-'''
-
-
-
-
